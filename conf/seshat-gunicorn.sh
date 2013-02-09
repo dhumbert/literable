1c67f610d0d0af93fad1737e2a9a8c7a86b351d8
Devin Humbert <devin.humbert@gmail.com>
2013-02-09 06:32:29 +0000

    Added some dependencies

---
+++ conf/seshat-gunicorn.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -e
+HOME=/var/www/seshat
+LOGFILE=$HOME/seshat.log
+NUM_WORKERS=10
+# user/group to run as
+USER=LOREM
+GROUP=IPSUM
+PORT=PORT
+cd $HOME
+source venv/bin/activate
+exec gunicorn seshat:app -kgevent -w $NUM_WORKERS -b 127.0.0.1:$PORT \
+    --user=$USER --group=$GROUP --log-level=error \
+    --log-file=$LOGFILE 2>>$LOGFILE
