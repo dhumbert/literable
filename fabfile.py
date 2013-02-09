3cd997f3dc68778042af7d2fce7d526756d4928d
Devin Humbert <devin.humbert@gmail.com>
2013-02-08 22:45:52 -0800

    Added deploy fabric task

--- fabfile.py
+++ fabfile.py
@@ -1,1 +1,9 @@
-# pip install -r requirements.txt
+from fabric.api import *
+
+
+def deploy():
+    sudo('stop seshat')
+    with cd('/var/www/seshat'):
+        run('git pull')
+        run('. venv/bin/activate && pip install -r requirements.txt')
+    sudo('start seshat')
