958b0401676ae27e76e4d09782b9ee8a8d7ef171
Devin Humbert <devin.humbert@gmail.com>
2013-10-12 15:22:48 -0700

    Removed unnecessary config params

--- seshat/config.py
+++ seshat/config.py
@@ -1,8 +1,6 @@
 LIBRARY_PATH = "../library/"
 SECRET_KEY = "=JbX0gvs3ivIZkST+HI"
 SQLALCHEMY_DATABASE_URI = "sqlite:///../library.db"
-USERNAME = "books"
-PASSWORD = "books"
 BOOKS_PER_PAGE = 5
 EBOOK_EXTENSIONS = ('epub', 'pdf')
 WRITE_META_ON_SAVE = True
