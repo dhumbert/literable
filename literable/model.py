7d73c13355ca32242861bd28dbd19010e05f5bd6
Devin Humbert <devin.humbert@gmail.com>
2014-10-26 21:33:07 -0700

    Hotfix for ES

--- literable/model.py
+++ literable/model.py
@@ -197,10 +197,10 @@ def book_to_elasticsearch(book):
                   'id': book.id,
                   }
         if book.series:
-            es_doc['series'] = book.series.name
+            es_doc['series'] = book.series[0].name
 
-        if book.author:
-            es_doc['author'] = book.author.name
+        if book.authors:
+            es_doc['author'] = book.authors[0].name
 
         es = get_es(urls=app.config['ELASTICSEARCH_NODES'])
         es.index(app.config['ELASTICSEARCH_INDEX'],
