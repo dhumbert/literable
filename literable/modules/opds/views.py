6ccdefcb1402b666d6dfc9f9f85a0fb17c74ef58
Devin Humbert <devin+git@d16.me>
2015-10-07 23:17:47 -0700

    Add Open Search for OPDS catalog.

--- literable/modules/opds/views.py
+++ literable/modules/opds/views.py
@@ -11,3 +11,16 @@ def index():
     page = request.args.get('page')
     books = model.get_recent_books(page, 'created', 'desc')
     return render_template('feed.xml', books=books)
+
+
+@opds_module.route('/search-manifest.xml')
+@basic_auth
+def search_manifest():
+    return render_template('search_manifest.xml')
+
+
+@opds_module.route('/search.atom')
+@basic_auth
+def search():
+    books = model.search_books(request.args.get('q'))
+    return render_template('feed.xml', books=books)
