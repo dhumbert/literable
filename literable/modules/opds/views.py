a30384df99480aeffbf8eb48234f11bb33790b34
Devin Humbert <devin+git@d16.me>
2015-10-09 17:02:45 -0700

    Flesh out OPDS catalog a bit

--- literable/modules/opds/views.py
+++ literable/modules/opds/views.py
@@ -1,16 +1,36 @@
 from flask import render_template, request
+from flask.ext.login import current_user
 
 from literable import model, basic_auth
 from literable.modules.opds import opds_module
 
 
-@opds_module.route('/')
-@opds_module.route('/recent.atom')
+@opds_module.route('/catalog.atom')
 @basic_auth
 def index():
+    return render_template('catalog.xml')
+
+
+@opds_module.route('/recent.atom')
+@basic_auth
+def recent():
     page = request.args.get('page')
     books = model.get_recent_books(page, 'created', 'desc')
-    return render_template('feed.xml', books=books)
+    return render_template('recent.xml', books=books)
+
+
+@opds_module.route('/lists.atom')
+@basic_auth
+def lists():
+    reading_lists = current_user.reading_lists
+    return render_template('lists.xml', lists=reading_lists)
+
+
+@opds_module.route('/list.atom')
+@basic_auth
+def list():
+    current_list = current_user.get_reading_list_by_id(int(request.args.get('id')))
+    return render_template('list.xml', list=current_list)
 
 
 @opds_module.route('/search-manifest.xml')
@@ -23,4 +43,4 @@ def search_manifest():
 @basic_auth
 def search():
     books = model.search_books(request.args.get('q'))
-    return render_template('feed.xml', books=books)
+    return render_template('recent.xml', books=books)
